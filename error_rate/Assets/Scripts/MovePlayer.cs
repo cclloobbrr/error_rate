316a9d49db5b8a5dfc3672ff294f1f8fc8f5c3a1
cclloobbrr <vvolodia479@gmail.com>
2023-06-16 18:42:23 +0300

    Поворот письки

--- error_rate/Assets/Scripts/MovePlayer.cs
+++ error_rate/Assets/Scripts/MovePlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public float speed = 5f;
+    public float speed = 0.02f, rotatespeed = 1f;
     private void Update() 
     {
         if(Input.GetKey(KeyCode.W))
@@ -18,5 +18,11 @@ public class MovePlayer : MonoBehaviour
 
         if(Input.GetKey(KeyCode.D))
             transform.Translate(new Vector2(1, 0) * speed);
+        
+        if(Input.GetKey(KeyCode.Q))
+            transform.Rotate(new Vector3(0, 0, 1) * rotatespeed);
+        
+        if(Input.GetKey(KeyCode.E))
+            transform.Rotate(new Vector3(0, 0, -1) * rotatespeed);
     }
 }
