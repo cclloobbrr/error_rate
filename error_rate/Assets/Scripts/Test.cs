44a4ca3215a86c5ea2f375ccc6c4d840de2d06af
cclloobbrr <vvolodia479@gmail.com>
2023-06-16 04:29:24 +0300

    wind commit
    Писюн сдувает

---
+++ error_rate/Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public GameObject CHLEN;
+
+    public Transform spe;
+
+    float speed = -0.02f;
+
+    private void Start() 
+    {
+
+    }
+
+    private void Update() 
+    {
+        spe.Translate(new Vector2(1, 0) * speed);
+    }
+}
