97e3742931d827feb2ff65a7e07cce2231735bc9
Ilya Gorshkov <XePaCeee@yandex.ru>
2023-06-21 23:08:48 +0300

    -

--- error_rate/Assets/Scripts/Fly.cs
+++ error_rate/Assets/Scripts/Fly.cs
@@ -8,12 +8,16 @@ public class Fly : MonoBehaviour
     public GameObject obj;
     float speed = 0.1f;
     public  Transform fly;
+    public int y;
+    public int x;
+
+
 
 
 
     private void Update() 
     {
-        fly.Translate(new Vector2(1, 0) * speed);        
+        fly.Translate(new Vector2(x, y) * speed);        
     }
-
+        
 }
